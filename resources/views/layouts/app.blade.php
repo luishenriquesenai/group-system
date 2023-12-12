f04aeca6ef2b4df3a49ae4e0469f530b90314776
oGuilherme1 <guilhermegoncalves1314@gmail.com>
2023-12-11 21:35:48 -0300

    Subindo alteracos do logout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,13 +10,14 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.css" rel="stylesheet" />
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+
 
             <!-- Page Heading -->
 
@@ -33,5 +34,6 @@
                 {{ $slot }}
             </main>
         </div>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.0/flowbite.min.js"></script>
     </body>
 </html>
