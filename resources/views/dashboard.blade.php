edd4a8ac92fe2fcee1de5a3236d87180608c3135
oGuilherme1 <guilhermegoncalves1314@gmail.com>
2023-12-12 22:29:36 -0300

    Subindo layout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,15 +13,61 @@
                     </div>
                 </div>
 
-                <nav class="mt-10 ">
-                    <a class="flex items-center px-6 py-2 mt-4 text-white bg-gray-500 bg-opacity-30" href="#">
-                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
-                        </svg>
+                <div class="p-0.5 bg-verde_logo"></div>
 
-                        <span class="mx-3">Clientes</span>
-                    </a>
+                <nav class="mt-2">
+                    <div class="py-4 overflow-y-auto">
+                        <ul class="space-y-2 font-medium">
+                            <li>
+                                <a href="#" onclick="replaceComponent('/customers')" class="flex items-center p-3 text-white dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                                        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
+                                        <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
+                                    </svg>
+                                    <span class="ms-3">Teste</span>
+                                </a>
+                            </li>
+                            <li>
+                                <button type="button" class="flex items-center w-full p-3 text-base text-white transition duration-75  group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500 transition duration-75  dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" viewBox="0 0 448 512" fill="currentColor">
+                                        <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                                    </svg>
+                                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Cadastro</span>
+                                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4" />
+                                    </svg>
+                                </button>
+                                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                                    <li>
+                                        <a href="#" class="flex items-center w-full p-3 text-white transition duration-75  pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Products</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center w-full p-3 text-white transition duration-75  pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Billing</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center w-full p-3 text-white transition duration-75  pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">Invoice</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center p-3 text-white  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                                        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
+                                    </svg>
+                                    <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="flex items-center p-3 text-white  dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-white dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
+                                        <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
+                                    </svg>
+                                    <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>
 
 
                 </nav>
@@ -31,12 +77,11 @@
                 <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
                     <div class="container px-6 py-8 mx-auto">
                         <div class="flex flex-col mt-8">
-                        <x-option-button/>
-                            <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                            <x-option-button />
+                            <div class="inline-block min-w-full border-black border-2 mt-14"></div>
+                            <div class="py-2 -my-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8" id="render-table">
 
-                                <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg">
-                                    
-                                </div>
+
                             </div>
                         </div>
                     </div>
@@ -46,3 +91,17 @@
     </div>
 
 </x-app-layout>
+
+<script>
+    function replaceComponent(route) {
+        const renderTables = document.getElementById("render-table");
+
+        fetch(route).then((response) => {
+            response.json().then((data) => {
+                console.log(data)
+                renderTables.replaceWith(data);
+            });
+        });
+    }
+
+</script>
