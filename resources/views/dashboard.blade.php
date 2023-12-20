9c8f042a041bcd10d1dc2d04765af22aadf7cce0
Guilherme <guilhermegoncalves1314@gmail.com>
2023-12-20 09:34:24 -0300

    tirando delete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -630,9 +630,6 @@
         class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
         <div class="relative p-4 w-full max-w-md max-h-full">
             <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <!-- <form action="{{ route('sua.rota.delete', $seuModel->id) }}" method="POST">
-                @csrf
-                @method('DELETE') -->
                 <div class="p-4 md:p-5 text-center">
                     <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
